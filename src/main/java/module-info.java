1744bc7f36c26d80b598cbff69f3e64456e24cb5
Nickolas Wood <53903568+Qualia91@users.noreply.github.com>
2020-11-10 20:23:32 +0000

    the great refactoring of lockdown part 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.nick.wood.game_engine.examples {
-	requires com.nick.wood.game_engine.gcs_model;
-	requires com.nick.wood.graphics_library;
-	requires com.nick.wood.maths;
-	requires com.nick.wood.game_engine.core;
-	requires com.nick.wood.game_engine.event_bus;
-	requires com.nick.wood.game.engine.systems;
-	requires com.nick.wood.physics_library;
-	exports com.nick.wood.game_engine.examples;
+module com.boc_dev.lge_examples {
+	requires com.boc_dev.lge_model;
+	requires com.boc_dev.graphics_library;
+	requires com.boc_dev.maths;
+	requires com.boc_dev.lge_core;
+	requires com.boc_dev.event_bus;
+	requires com.boc_dev.lge_systems;
+	requires com.boc_dev.physics_library;
+	exports com.boc_dev.lge_examples;
 }
